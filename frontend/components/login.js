2ec2197302d82165245459ba1d19195daf0437c1
JuliaNguyen1220 <108833757+JuliaNguyen1220@users.noreply.github.com>
2023-09-08 06:46:20 +0000

    cs628-hos10-nguyengiang

---
+++ frontend/components/login.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <a href="https://curly-space-rotary-phone-gj5rrvg79xvcvjrw-5050.app.github.dev/facebook" 
+    className="btn btn-primary">
+      Login with Facebook
+    </a>
+  );
+};
+
+export default Login;
